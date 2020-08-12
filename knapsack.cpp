62a78d0decbdfe9f030e5cce64b980b4cf3355b6
rrichards21 <rodrigo.a.richards@gmail.com>
2020-08-12 18:48:30 -0400

    first commit

---
+++ knapsack.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; //cantidad de objetos
+    int W; //capacidad de mochila
+
+    //vector<int> objetos(n);   //vector de objetos a añadir
+    //vector<int> B(n); //vector binario (0 no está en mochila, 1 está en mochila)
+
+    return 0;
+}
